fee849245fc243072748e3dcf5a8b72d27592a40
Oluwabamise Olatunji <tunjibamise94@gmail.com>
2022-09-13 03:29:35 +0100

    form submission updates

--- js/mail.js
+++ js/mail.js
@@ -1,28 +1,2 @@
-const form = document.getElementById('form');
-const formName = document.getElementById('name');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const message = document.getElementById('message');
 
 
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    Email.send({
-	Host: 'smtp.gmail.com',
-	Username: 'tunjibamise94@gmail.com',
-	Password: '@94Oluwabamise',
-	To: 'tunjibamise94@gmail.com',
-	From: `${email.value}`,
-	Subject: `${formName.value}`,
-	Body: `Name: ${formName.value} <br/> Email: ${email.value} <br/> Phone: ${phone.value} <br/> Message: ${message.value}`
-    }).then((message) => alert(message));
-
-    formName.value = '';
-    email.value = '';
-    phone.value = '';
-    message.value = '';
-
-});
-
-    
